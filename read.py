eb5c5367702496dc4afa47d95670944e69cfd7d1
Derek Farr <derek@devbox.jackpot.arpa>
2024-05-26 17:52:11 -0400

    refactor

---
+++ read.py
@@ -0,0 +1,13 @@
+from psycopg2 import sql
+
+
+def fetch_from_db(connection, table_name: str, column_names: tuple[str]):
+    query = sql.SQL("SELECT {columns} FROM {table};").format(
+        columns=sql.SQL(', ').join(map(sql.Identifier, column_names)),
+        table=sql.Identifier(table_name))
+
+    with connection.cursor() as cursor:
+        cursor.execute(query)
+        data = cursor.fetchall()
+        
+    return data
