8dffaf6055b2c63744f4034a16e381c95b0c95a1
Derek Farr <derek@devbox.jackpot.arpa>
2024-05-27 14:34:38 -0400

    plot vehicle paths on map

--- read.py
+++ read.py
@@ -1,11 +1,28 @@
 from psycopg2 import sql
 
 
-def from_db(connection, table_name: str, column_names: tuple[str]):
-    query = sql.SQL("SELECT {columns} FROM {table};").format(
-        columns=sql.SQL(', ').join(map(sql.Identifier, column_names)),
-        table=sql.Identifier(table_name))
+def vehicle_positions(connection, table_name: str):
+    query = sql.SQL("""
+            SELECT timestamp, vehicle_id, longitude, latitude, trip_id
+            FROM {table}
+            ORDER BY trip_id;
+        """).format(table=sql.Identifier(table_name))
+    
+    return from_db(connection, query)
 
+
+def vehicle_positions_by_trip(connection, table_name:str):
+    query = sql.SQL("""
+            SELECT trip_id, vehicle_id, COUNT(*) AS trip_count, array_agg()
+            FROM {table}
+            GROUP BY trip_id, vehicle_id
+            ORDER BY trip_count DESC;
+        """).format(table=sql.Identifier(table_name))
+    
+    return from_db(connection, query)
+
+
+def from_db(connection, query):
     with connection.cursor() as cursor:
         cursor.execute(query)
         data = cursor.fetchall()
